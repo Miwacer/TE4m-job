d785c0deafdfed54d07f07661184bcd6439c1014
Miwacer <il>
2025-05-14 23:32:00 +0300

    add TaskList & TaskDetail to views

--- workspace/urls.py
+++ workspace/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from .views import index
+from .views import (
+    index,
+TaskListView,
+TaskDetailView,
+
+)
+
 
 app_name = "workspace"
 urlpatterns = [
     path("", index, name="index"),
+    path("tasks/", TaskListView.as_view(), name="task-list"),
+    path("tasks/<int:pk>", TaskDetailView.as_view(), name="task-detail"),
 ]
--- workspace/views.py
+++ workspace/views.py
@@ -1,5 +1,21 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views import generic
+from .models import Task, TaskType, Worker, Position
+
 
 def index(request: HttpRequest) -> HttpResponse:
-    return render(request, 'base.html')
+    user = request.user
+
+    context = {
+        "user": user,
+    }
+    return render(request, 'workspace/index.html', context)
+
+
+class TaskListView(generic.ListView):
+    model = Task
+
+
+class TaskDetailView(generic.DetailView):
+    model = Task
