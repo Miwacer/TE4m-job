3a99179c4d48afdf186d2bff96356af6901248de
Miwacer <il>
2025-05-14 14:23:37 +0300

    add model Position

--- workspace/models.py
+++ workspace/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+class Position(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Worker(AbstractUser):
     position = models.ForeignKey(
         "Position",
