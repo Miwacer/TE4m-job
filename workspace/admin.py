6d168aef47c6ba9ebc8b9f1e55aa66f34eee4c64
Miwacer <il>
2025-05-14 18:06:41 +0300

    Add templates and static

--- workspace/admin.py
+++ workspace/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import (
+    Worker,
+    Task,
+    TaskType,
+    Position,
+)
 
-# Register your models here.
+
+class AuthorAdmin(admin.ModelAdmin):
+    site_header = "Monty Python administration"
+
+
+admin.site.register(Task)
+admin.site.register(TaskType)
+admin.site.register(Position)
+admin.site.register(Worker, AuthorAdmin)
--- workspace/models.py
+++ workspace/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Position(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100, unique=True, null=True)
 
     def __str__(self):
         return self.name
@@ -20,6 +20,7 @@ class Worker(AbstractUser):
         "Position",
         on_delete=models.PROTECT,
         related_name="workers",
+        null = True,
     )
 
 
@@ -41,4 +42,3 @@ class Task(models.Model):
         'Worker',
         related_name="assignees",
     )
-    
