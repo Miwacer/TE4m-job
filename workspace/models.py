60f879fd0150e1bf7c0e32ddaba48c0acd9bc924
Miwacer <il>
2025-05-14 16:39:24 +0300

    add models TaskType, Task

--- workspace/models.py
+++ workspace/models.py
@@ -8,9 +8,37 @@ class Position(models.Model):
         return self.name
 
 
+class TaskType(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Worker(AbstractUser):
     position = models.ForeignKey(
         "Position",
         on_delete=models.PROTECT,
         related_name="workers",
     )
+
+
+
+class Task(models.Model):
+    STATUS = (
+        ("UR", "Urgent"),
+        ("HI", "High"),
+        ("MD", "Medium"),
+        ("LW", "Low")
+    )
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    is_complete = models.BooleanField(default=False)
+    priority = models.CharField(max_length=2, choices=STATUS)
+    task_type = models.ForeignKey(TaskType, on_delete=models.PROTECT)
+    assignees = models.ManyToManyField(
+        'Worker',
+        related_name="assignees",
+    )
+    
