6fdb3568676df7019ca91bacf76da3cca0ac5a5e
Miwacer <il>
2025-05-17 18:48:09 +0300

    optimization

--- workspace/urls.py
+++ workspace/urls.py
@@ -35,16 +35,44 @@ urlpatterns = [
          name="task-type-detail"
     ),
     path("positions/", PositionListView.as_view(), name="position-list"),
-    path("position/<int:pk>", PositionDetailView.as_view(), name="position-detail"),
+    path(
+        "position/<int:pk>",
+        PositionDetailView.as_view(),
+        name="position-detail"
+    ),
     path("workers/", WorkerListView.as_view(), name="worker-list"),
-    path("worker/<int:pk>", WorkerDetailView.as_view(), name="worker-detail"),
+    path(
+        "worker/<int:pk>",
+        WorkerDetailView.as_view(),
+        name="worker-detail"
+    ),
     path("projects/", ProjectListView.as_view(), name="project-list"),
-    path("project/create/", ProjectCreateView.as_view(), name="project-create"),
-    path("project/<int:pk>", ProjectDetailView.as_view(), name="project-detail"),
+    path(
+        "project/create/",
+         ProjectCreateView.as_view(),
+        name="project-create"
+    ),
+    path(
+        "project/<int:pk>",
+        ProjectDetailView.as_view(),
+        name="project-detail"
+    ),
     path("teams/", TeamListView.as_view(), name="team-list"),
     path("team/<int:pk>", TeamDetailView.as_view(), name="team-detail"),
     path("team/create/", TeamCreateView.as_view(), name="team-create"),
-    path("team/<int:pk>/update", TeamUpdateView.as_view(), name="team-update"),
-    path("team/<int:pk>/delete", TeamDeleteView.as_view(), name="team-delete"),
-    path("remove-member/<int:team_id>/<int:member_id>/", delete_member_from_team, name="remove-member"),
+    path(
+        "team/<int:pk>/update",
+        TeamUpdateView.as_view(),
+        name="team-update"
+    ),
+    path(
+        "team/<int:pk>/delete",
+        TeamDeleteView.as_view(),
+        name="team-delete"
+    ),
+    path(
+        "remove-member/<int:team_id>/<int:member_id>/",
+        delete_member_from_team,
+        name="remove-member"
+    ),
 ]
